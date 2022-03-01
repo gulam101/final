2c764183cac921611c168c0c31ca38db3e942b01
Gulam <gfram@hotmail.co.uk>
2022-03-01 14:10:07 +0000

    Updated Imgs, Reworked Homepage, Updated Toolbar and Updated CSS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Global
+var begginer = "Train begginer ";
+// Correct spelling for this below later
+var intermediate = "Train intermediate ";
+var advanced = "Train advanced ";
+document.getElementById("demo").innerHTML =
+
+// Notification Menu
+
+// Cookie consent
+
+// Modal script
