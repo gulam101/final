18220bc523f74e19dca3292be3e66341197776e1
Gulam <gfram@hotmail.co.uk>
2022-03-08 15:42:39 +0000

    Updated Settings Page, Updated JS file, Removed Scripts on .html pages, Updated CSS and fixed Footer bug in Settings Page

--- js/main.js
+++ js/main.js
@@ -3,10 +3,37 @@ var begginer = "Train begginer ";
 // Correct spelling for this below later
 var intermediate = "Train intermediate ";
 var advanced = "Train advanced ";
-document.getElementById("demo").innerHTML =
+// document.getElementById("demo").innerHTML =
+
+// navbar function
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
 
 // Notification Menu
 
 // Cookie consent
 
 // Modal script
+var modal = document.getElementById('id01');
+      
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+// Google Captcha
+function onSubmit(token) {
+    document.getElementById("demo-form").submit();
+}
